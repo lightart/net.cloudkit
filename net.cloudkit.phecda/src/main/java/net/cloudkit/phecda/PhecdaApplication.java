e73d7a8b542b8b9ff067a62e5c31f11b210ad094
icloudkit <hongquanli@qq.com>
2016-09-06 16:07:05 +0800

    //

---
+++ net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/PhecdaApplication.java
@@ -0,0 +1,12 @@
+package net.cloudkit.phecda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhecdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhecdaApplication.class, args);
+	}
+}
