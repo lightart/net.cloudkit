591b89ac8a6f2b21fa15eefe499e167d09166d08
icloudkit <hongquanli@qq.com>
2016-09-07 09:20:05 +0800

    //

--- net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/PhecdaApplication.java
+++ net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/PhecdaApplication.java
@@ -2,7 +2,19 @@ package net.cloudkit.phecda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EntityScan("net.cloudkit.phecda.domain.model")
+@EnableJpaRepositories(basePackages = "net.cloudkit.phecda.domain")
+@EnableTransactionManagement
+
+// @Configuration
+// @EnableAutoConfiguration
+// @ComponentScan(basePackages = "net.cloudkit.ecosystem")
+
+// @SpringBootApplication same as @Configuration @EnableAutoConfiguration @ComponentScan
 @SpringBootApplication
 public class PhecdaApplication {
 
---
+++ net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/interfaces/SimpleController.java
@@ -0,0 +1,23 @@
+package net.cloudkit.phecda.interfaces;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * SimpleController.java
+ */
+// @RestController
+@Controller
+@EnableAutoConfiguration
+public class SimpleController {
+
+    @RequestMapping(value ="/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(){
+        return "hello world";
+    }
+
+}
