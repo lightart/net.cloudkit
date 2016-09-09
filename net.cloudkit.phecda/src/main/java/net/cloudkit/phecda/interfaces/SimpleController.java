979aa0c2f8221276d11e43b285ea37fc251c5926
icloudkit <hongquanli@qq.com>
2016-09-09 22:32:39 +0800

    //

--- net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/interfaces/SimpleController.java
+++ net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/interfaces/SimpleController.java
@@ -2,6 +2,7 @@ package net.cloudkit.phecda.interfaces;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,8 +18,9 @@ public class SimpleController {
 
     @RequestMapping(value ="/hello", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String hello(){
-        return "hello world";
+    public Model hello(Model model){
+        model.addAttribute("hello", "123456");
+        return model;
     }
 
 }
