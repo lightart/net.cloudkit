4d042dfda361553bc9b619d8bec2f2fc78ab3eed
icloudkit <hongquanli@qq.com>
2016-09-09 18:51:37 +0800

    //

--- net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/infrastructure/configuration/WebMvcConfiguration.java
+++ net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/infrastructure/configuration/WebMvcConfiguration.java
@@ -1,16 +1,13 @@
 package net.cloudkit.phecda.infrastructure.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 // WebMvcConfigurationSupport WebMvcConfigurerAdapter
 @EnableWebMvc
-@ComponentScan(basePackageClasses = { MyConfiguration.class })
-public class MyConfiguration extends WebMvcConfigurationSupport {
+@ComponentScan(basePackageClasses = { WebMvcConfiguration.class })
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 //
 //    @Override
 //    public void addFormatters(FormatterRegistry formatterRegistry) {
