9d12f220731566093236da05bea90781c8a092bb
icloudkit <hongquanli@qq.com>
2016-09-17 09:48:13 +0800

    //

--- net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/infrastructure/configuration/WebMvcConfiguration.java
+++ net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/infrastructure/configuration/WebMvcConfiguration.java
@@ -1,16 +1,42 @@
-//package net.cloudkit.phecda.infrastructure.configuration;
-//
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//// WebMvcConfigurationSupport WebMvcConfigurerAdapter
-//// @ComponentScan(basePackageClasses = { WebMvcConfiguration.class })
-//@EnableWebMvc
-//@Configuration
-//public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+package net.cloudkit.phecda.infrastructure.configuration;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.method.annotation.MapMethodProcessor;
+import org.springframework.web.method.annotation.ModelAttributeMethodProcessor;
+import org.springframework.web.method.annotation.ModelMethodProcessor;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.*;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+// WebMvcConfigurationSupport WebMvcConfigurerAdapter WebMvcAutoConfiguration
+// @ComponentScan(basePackageClasses = { WebMvcConfiguration.class })
+@EnableWebMvc
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 ////
 ////    @Override
 ////    public void addFormatters(FormatterRegistry formatterRegistry) {
@@ -24,17 +50,59 @@
 //
 //    // More overridden methods ...
 
-//@Override
-//public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//    configurer.enable();
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
+
+    @Bean
+    @ConditionalOnBean(ViewResolver.class)
+    @ConditionalOnMissingBean(name = "viewResolver", value = ContentNegotiatingViewResolver.class)
+    public ContentNegotiatingViewResolver viewResolver(BeanFactory beanFactory) {
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setContentNegotiationManager(beanFactory.getBean(ContentNegotiationManager.class));
+        // ContentNegotiatingViewResolver uses all the other view resolvers to locate
+        // a view so it should have a high precedence
+        MappingJackson2JsonView mappingJackson2JsonView = beanFactory.getBean(MappingJackson2JsonView.class);
+        mappingJackson2JsonView.setPrettyPrint(false);
+        mappingJackson2JsonView.setDisableCaching(true);
+        Set<String> jsonParameterNames = new HashSet<>();
+        jsonParameterNames.add("jsonp");
+        jsonParameterNames.add("callback");
+        mappingJackson2JsonView.setJsonpParameterNames(jsonParameterNames);
+        List<View> defaultViews = new ArrayList<>();
+        defaultViews.add(mappingJackson2JsonView);
+        resolver.setDefaultViews(defaultViews);
+        resolver.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return resolver;
+    }
+
+
+
+//    @Bean
+//    @ConditionalOnBean({WebMvcProperties.View.class})
+//    @ConditionalOnMissingBean
+//    public BeanNameViewResolver beanNameViewResolver() {
+//        BeanNameViewResolver resolver = new BeanNameViewResolver();
+//        resolver.setOrder(2147483637);
+//        return resolver;
 //    }
 //
-//@Bean
-//public InternalResourceViewResolver getViewResolver(){
-//    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//    viewResolver.setPrefix("/WEB-INF/jsp/");
-//    viewResolver.setSuffix(".jsp");
-//    return viewResolver;
-//}
-
-//}
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public InternalResourceViewResolver defaultViewResolver() {
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setPrefix(this.mvcProperties.getView().getPrefix());
+//        esolver.setSuffix(this.mvcProperties.getView().getSuffix());
+//        return resolver;
+//    }
+//
+//    @Bean
+//    public InternalResourceViewResolver getViewResolver() {
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setPrefix("/WEB-INF/jsp/");
+//        viewResolver.setSuffix(".jsp");
+//        viewResolver.setOrder(1);
+//        return viewResolver;
+//    }
+}
--- net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/interfaces/ServiceController.java
+++ net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/interfaces/ServiceController.java
@@ -1,15 +1,22 @@
 package net.cloudkit.phecda.interfaces;
 
 import net.cloudkit.phecda.domain.model.shared.City;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 /**
@@ -20,10 +27,26 @@ import java.util.*;
 //@Controller
 public class ServiceController {
 
+//    public static HttpHeaders httpHeaderExcelFileAttachment(final String fileName,
+//                                                            final int fileSize) {
+//        String encodedFileName = fileName.replace('"', ' ').replace(' ', '_');
+//
+//        HttpHeaders responseHeaders = new HttpHeaders();
+//        responseHeaders.setContentType(MediaType.parseMediaType("application/vnd.ms-excel"));
+//        responseHeaders.setContentLength(fileSize);
+//        responseHeaders.set("Content-Disposition", "attachment");
+//        responseHeaders.add("Content-Disposition", "filename=\"" + encodedFileName + '\"');
+//        return responseHeaders;
+//    }
+
     // @PathVariable String version, @PathVariable String extension
     // params="myParam=myValue"
     // headers="Referer=http://www.simple.com/"
     // , RedirectAttributes redirectAttributes
+    // ResponseEntity<Person>
+    // @PathVariable
+    // new ResponseEntity<byte[]>(file.getDataArray(), responseHeaders, HttpStatus.OK);
+    // HttpHeaders responseHeaders = httpHeaderExcelFileAttachment(fileName, datei.getDaten().length);
     @RequestMapping(
         value ={"/service", "/service.json"},
         method = {RequestMethod.GET, RequestMethod.POST},
@@ -118,7 +141,7 @@ public class ServiceController {
         return modelAndView;
     }
 
-    @RequestMapping(value = "/find")
+    @RequestMapping(value = "/find", produces="application/json")
     //@ResponseBody
     public Model find(Model model) {
         City city = new City("BeiJing", "China");
@@ -140,5 +163,32 @@ public class ServiceController {
     @RequestMapping({"/", "/home", "index"})
     public String home() {
         return "/index";
+    }
+
+    @RequestMapping({"/map"})
+    public ModelMap map() {
+        return new ModelMap("status", "ok");
+    }
+
+    @RequestMapping("/something")
+    public ResponseEntity<String> handle(HttpEntity<byte[]> requestEntity) throws UnsupportedEncodingException {
+        String requestHeader = requestEntity.getHeaders().getFirst("MyRequestHeader");
+        byte[] requestBody = requestEntity.getBody();
+
+        // do something with request header and body
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("MyResponseHeader", "MyValue");
+        return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/content", produces="application/json")
+    @ResponseBody
+    public ModelAndView getContent() {
+        ModelAndView mav = new ModelAndView();
+        // mav.setViewName("content");
+        mav.addObject("sampleContentList", "test");
+        // return new ModelAndView(new MappingJackson2JsonView(), new HashMap<String, String>());
+        return mav;
     }
 }
