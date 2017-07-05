ef78f350640f5bb4aa861e68d4d29a9b2fa42dc6
icloudkit <hongquanli@qq.com>
2017-07-05 08:56:16 +0800

    //

---
+++ net.cloudkit.transform/src/main/java/net/cloudkit/transform/FutureTaskTest.java
@@ -0,0 +1,33 @@
+package net.cloudkit.transform;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * http://www.jianshu.com/p/1dd951412ff5
+ */
+public class FutureTaskTest {
+    public static void main(String[] args) throws InterruptedException{
+        FutureTask<Integer> ft = new FutureTask<>(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                int num = new Random().nextInt(10);
+                TimeUnit.SECONDS.sleep(num);
+                return num;
+            }
+        });
+        Thread t = new Thread(ft);
+        t.start();
+
+        //这里可以做一些其它的事情，跟futureTask任务并行，等需要futureTask的运行结果时，可以调用get方法获取。
+        try {
+            //等待任务执行完成，获取返回值
+            Integer num = ft.get();
+            System.out.println(num);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
