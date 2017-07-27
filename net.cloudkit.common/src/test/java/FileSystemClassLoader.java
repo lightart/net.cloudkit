034dadf80746c1aba91c76bb3068c4eae3db93d4
icloudkit <hongquanli@qq.com>
2017-07-27 11:03:54 +0800

    //

--- net.cloudkit.common/src/test/java/FileSystemClassLoader.java
+++ net.cloudkit.common/src/test/java/FileSystemClassLoader.java
@@ -1,5 +1,22 @@
 import java.io.*;
 
+/**
+ * 引导类加载器（bootstrap class loader）：
+ * 它用来加载 Java 的核心库，是用原生代码来实现的，并不继承自 java.lang.ClassLoader。
+ *
+ * 扩展类加载器（extensions class loader）：
+ * 它用来加载 Java 的扩展库。Java 虚拟机的实现会提供一个扩展库目录。该类加载器在此目录里面查找并加载 Java 类。
+ *
+ * 系统类加载器（system class loader）：
+ * 它根据 Java 应用的类路径（CLASSPATH）来加载 Java 类。一般来说，Java 应用的类都是由它来完成加载的。可以通过 ClassLoader.getSystemClassLoader()来获取它。
+ *
+ * 加载器的树状组织结构
+ * ClassLoader loader = ClassLoaderTree.class.getClassLoader();
+ * while (loader != null) {
+ * System.out.println(loader.toString());
+ * loader = loader.getParent();
+ }
+ */
 public class FileSystemClassLoader extends ClassLoader {
 
     private String rootDir;
