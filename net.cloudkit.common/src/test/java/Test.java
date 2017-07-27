861f7c9e6bbcf41c7d131eb00e6b8c9331209a65
icloudkit <hongquanli@qq.com>
2017-07-27 10:08:37 +0800

    //

--- net.cloudkit.common/src/test/java/Test.java
+++ net.cloudkit.common/src/test/java/Test.java
@@ -48,5 +48,23 @@ public class Test {
         }
 
     }
+
+
+    public void testClassIdentity() {
+        String classDataRootPath = "C:\\workspace\\Classloader\\classData";
+        FileSystemClassLoader fscl1 = new FileSystemClassLoader(classDataRootPath);
+        FileSystemClassLoader fscl2 = new FileSystemClassLoader(classDataRootPath);
+        String className = "com.example.Sample";
+        try {
+            Class<?> class1 = fscl1.loadClass(className);
+            Object obj1 = class1.newInstance();
+            Class<?> class2 = fscl2.loadClass(className);
+            Object obj2 = class2.newInstance();
+            Method setSampleMethod = class1.getMethod("setSample", Object.class);
+            setSampleMethod.invoke(obj1, obj2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
