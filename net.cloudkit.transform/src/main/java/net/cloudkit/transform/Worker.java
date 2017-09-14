7541a763267763fb3fb27a67677f6093bcf7c9e7
icloudkit <hongquanli@qq.com>
2017-09-14 10:22:18 +0800

    //

--- net.cloudkit.transform/src/main/java/net/cloudkit/transform/Worker.java
+++ net.cloudkit.transform/src/main/java/net/cloudkit/transform/Worker.java
@@ -24,6 +24,7 @@ public class Worker {
         channel.basicQos(1);
 
         final Consumer consumer = new DefaultConsumer(channel) {
+
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
