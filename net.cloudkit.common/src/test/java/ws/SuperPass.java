9905da2d710578f195afb1972f7ba251f8552932
icloudkit <hongquanli@qq.com>
2017-10-13 17:25:11 +0800

    //

--- net.cloudkit.common/src/test/java/ws/SuperPass.java
+++ net.cloudkit.common/src/test/java/ws/SuperPass.java
@@ -29,5 +29,10 @@ public interface SuperPass {
     @Action
     @WebMethod(action = "service", operationName = "service", exclude = false)
     @WebResult(name = "result")
-    byte[] service(@WebParam(name = "serviceName") String serviceName, @WebParam(name = "requestContext") byte[] requestContext, @WebParam(name = "requestData") byte[] requestData, @WebParam(name = "responseData", mode = WebParam.Mode.OUT) Holder<byte[]> responseData);
+    byte[] service(
+        @WebParam(name = "serviceName") String serviceName,
+        @WebParam(name = "requestContext") byte[] requestContext,
+        @WebParam(name = "requestData") byte[] requestData,
+        @WebParam(name = "responseData", mode = WebParam.Mode.OUT) Holder<byte[]> responseData
+    );
 }
