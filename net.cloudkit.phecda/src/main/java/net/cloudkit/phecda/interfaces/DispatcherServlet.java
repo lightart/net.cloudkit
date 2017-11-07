05fc0bbda86accc5b42fd2abc6c3128f975d5a8b
icloudkit <hongquanli@qq.com>
2017-11-07 10:59:28 +0800

    //

--- net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/interfaces/DispatcherServlet.java
+++ net.cloudkit.phecda/src/main/java/net/cloudkit/phecda/interfaces/DispatcherServlet.java
@@ -17,12 +17,18 @@ import java.io.IOException;
 public class DispatcherServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
 
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
 
     }
 }
