818b85524ca65f7991d3925dfe1fe6dd6b2647e4
icloudkit <hongquanli@qq.com>
2017-11-07 17:43:32 +0800

    //

--- net.cloudkit.common/src/test/java/observer/Student.java
+++ net.cloudkit.common/src/test/java/observer/Student.java
@@ -3,21 +3,21 @@ package observer;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Student implements Observer{
+public class Student implements Observer {
 
     private Observable ob;
     private String name;
 
-    public Student(String name,Observable ob) {
+    public Student(String name, Observable ob) {
         this.ob = ob;
-        this.name=name;
+        this.name = name;
         ob.addObserver(this);
     }
 
     @Override
     public void update(Observable o, Object arg) {
-        Teacher t=(Teacher)o;
-        System.out.println(name+"得到作业信息:"+t.getInfo());
+        Teacher t = (Teacher) o;
+        System.out.println(name + "得到作业信息:" + t.getInfo());
 
     }
 
